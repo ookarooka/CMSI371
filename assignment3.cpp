1dfd3d6f171376781d17c3367124ec2b7af7ddce
Josh Patterson <34358565+ookarooka@users.noreply.github.com>
2019-04-11 18:32:46 -0700

    Update assignment3.cpp

--- assignment3.cpp
+++ assignment3.cpp
@@ -184,43 +184,7 @@ vector<GLfloat> mat_mult(vector<GLfloat> A, vector<GLfloat> B) {
 // Builds a unit cube centered at the origin
 vector<GLfloat> build_cube() {
     vector<GLfloat> result;
-    vector<GLfloat> plane;
-    vector<GLfloat> front;
-    vector<GLfloat> left;
-    vector<GLfloat> right;
-    vector<GLfloat> back;
-    vector<GLfloat> top;
-    vector<GLfloat> bottom;
-    
-    plane = to_homogeneous_coord(init_plane());
-    
-    front = mat_mult(translation_matrix(0.0, 0.0, 0.5), plane);
-    left = mat_mult(rotation_matrix_y(-90.0), plane);
-    left = mat_mult(translation_matrix(-0.5, 0.0, 0.0), left);
-    right = mat_mult(rotation_matrix_y(90.0), plane);
-    right = mat_mult(translation_matrix(0.5, 0.0, 0.0), right);
-    back = mat_mult(rotation_matrix_y(180.0), plane);
-    back = mat_mult(translation_matrix(0.0, 0.0, -0.5), back);
-    top = mat_mult(rotation_matrix_x(-90.0), plane);
-    top = mat_mult(translation_matrix(0.0, 0.5, 0.0), top);
-    bottom = mat_mult(rotation_matrix_x(90.0), plane);
-    bottom = mat_mult(translation_matrix(0.0, -0.5, 0.0), bottom);
-    
-    
-    front = to_cartesian_coord(front);
-    left = to_cartesian_coord(left);
-    right = to_cartesian_coord(right);
-    back = to_cartesian_coord(back);
-    top = to_cartesian_coord(top);
-    bottom = to_cartesian_coord(bottom);
-    
-    result.insert(result.end(), front.begin(), front.end());
-    result.insert(result.end(), left.begin(), left.end());
-    result.insert(result.end(), right.begin(), right.end());
-    result.insert(result.end(), back.begin(), back.end());
-    result.insert(result.end(), top.begin(), top.end());
-    result.insert(result.end(), bottom.begin(), bottom.end());
-    
+
     return result;
 }
 
@@ -256,65 +220,7 @@ void init_camera() {
 // Construct the scene using objects built from cubes/prisms
 vector<GLfloat> init_scene() {
     vector<GLfloat> scene;
-    vector<GLfloat> homog_plane;
-    vector<GLfloat> couch_cushion;
-    vector<GLfloat> rt_couch_cushion;
-    vector<GLfloat> lf_couch_cushion;
-    vector<GLfloat> back_couch_cushion;
-    vector<GLfloat> tv;
-    vector<GLfloat> lamp_base;
-    vector<GLfloat> lamp_pole;
-    vector<GLfloat> lampshade;
-    vector<GLfloat> table_base;
-    vector<GLfloat> tabletop;
-    
-    homog_plane = to_homogeneous_coord(build_cube());
-    
-    // Scaling and Translating the points
-    couch_cushion = mat_mult(scaling_matrix(1.5, 0.5, 1.0), homog_plane);
-    rt_couch_cushion = mat_mult(scaling_matrix(0.5, 1.0, 1.0), homog_plane);
-    rt_couch_cushion = mat_mult(translation_matrix(1.0, 0.0, 0.0), rt_couch_cushion);
-    lf_couch_cushion = mat_mult(translation_matrix(-2.0, 0.0, 0.0), rt_couch_cushion);
-    back_couch_cushion = mat_mult(scaling_matrix(2.5, 2.0, 0.3), homog_plane);
-    back_couch_cushion = mat_mult(translation_matrix(0.0, 0.5, -0.65), back_couch_cushion);
-    tv = mat_mult(scaling_matrix(2.0, 1.3, 0.2), homog_plane);
-    tv = mat_mult(rotation_matrix_y(180), tv);
-    tv = mat_mult(translation_matrix(0.0, 0.75, 2.5), tv);
-    table_base = mat_mult(scaling_matrix(0.5, 0.5, 0.5), homog_plane);
-    table_base = mat_mult(translation_matrix(2.0, -0.25, 0.0), table_base);
-    tabletop = mat_mult(scaling_matrix(1.0, 0.25, 1.0), homog_plane);
-    tabletop = mat_mult(translation_matrix(2.0, 0.25, 0.0), tabletop);
-    lamp_base = mat_mult(scaling_matrix(1.0, 0.1, 1.0), homog_plane);
-    lamp_base = mat_mult(translation_matrix(-2.0, -0.45, 0.0), lamp_base);
-    lamp_pole = mat_mult(scaling_matrix(0.1, 3.0, 0.1), homog_plane);
-    lamp_pole = mat_mult(translation_matrix(-2.0, 1.0, 0.0), lamp_pole);
-    lampshade = mat_mult(scaling_matrix(1.0, 0.75, 1.0), homog_plane);
-    lampshade = mat_mult(translation_matrix(-2.0, 2.75, 0.0), lampshade);
-    
-    
-    // Cartesian
-    couch_cushion = to_cartesian_coord(couch_cushion);
-    rt_couch_cushion = to_cartesian_coord(rt_couch_cushion);
-    lf_couch_cushion = to_cartesian_coord(lf_couch_cushion);
-    back_couch_cushion = to_cartesian_coord(back_couch_cushion);
-    tv = to_cartesian_coord(tv);
-    table_base = to_cartesian_coord(table_base);
-    tabletop = to_cartesian_coord(tabletop);
-    lamp_base = to_cartesian_coord(lamp_base);
-    lamp_pole = to_cartesian_coord(lamp_pole);
-    lampshade = to_cartesian_coord(lampshade);
-    
-    // Adding to the scene
-    scene.insert(scene.end(), couch_cushion.begin(), couch_cushion.end());
-    scene.insert(scene.end(), rt_couch_cushion.begin(), rt_couch_cushion.end());
-    scene.insert(scene.end(), lf_couch_cushion.begin(), lf_couch_cushion.end());
-    scene.insert(scene.end(), back_couch_cushion.begin(), back_couch_cushion.end());
-    scene.insert(scene.end(), tv.begin(), tv.end());
-    scene.insert(scene.end(), table_base.begin(), table_base.end());
-    scene.insert(scene.end(), tabletop.begin(), tabletop.end());
-    scene.insert(scene.end(), lamp_base.begin(), lamp_base.end());
-    scene.insert(scene.end(), lamp_pole.begin(), lamp_pole.end());
-    scene.insert(scene.end(), lampshade.begin(), lampshade.end());
+  
     return scene;
 }
 
@@ -336,6 +242,7 @@ void display_func() {
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
     
+    int scene_size = SCENE.size()/16;    
     //ROTATIONS
     vector<GLfloat> pointValues = to_homogeneous_coord(init_scene());
     pointValues = mat_mult(rotation_matrix_y(THETA), pointValues);
@@ -359,8 +266,8 @@ void display_func() {
                    pointColor);     // Pointer to memory location to read from
     
     // Draw quad point planes: each 4 vertices
-    glDrawArrays(GL_QUADS, 0, 4 * 60);
-    
+    glDrawArrays(GL_QUADS, 0, 4 * scene_size);
+    ///instead of scene_size you could use 60
     glFlush();            //Finish rendering
     glutSwapBuffers();
 }
@@ -396,4 +303,3 @@ int main (int argc, char **argv) {
     
     return 0;
 }
-
